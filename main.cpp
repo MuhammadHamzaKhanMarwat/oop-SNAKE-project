353b0bf2882ba3707f4fbc7beec52a5630ecc118
Muhammad Hamza Khan <p229178@pwr.nu.edu.pk>
2023-11-28 20:16:38 +0500

    Basic Window file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
+using namespace sf;
 int main()
 {
+	RenderWindow window(VideoMode(100, 100), "Snake");
+	while (window.isOpen())
+	{
+		Event E;
+		while(window.pollEvent(E))
+		{
+			if (E.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+		window.clear();
+		window.display();
+	}
 	return 0;
 }
